51afc52f865c71ca0ef5b47e26f65e57969c4f5c
John Chang <johnchang9094@gmail.com>
2018-11-20 23:36:12 +0000

    URI abbreviation via hashing

---
+++ src/main/java/com/johnc/urlabbreviator/UrlAbbreviatorApplication.java
@@ -0,0 +1,12 @@
+package com.johnc.urlabbreviator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlAbbreviatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlAbbreviatorApplication.class, args);
+	}
+}
